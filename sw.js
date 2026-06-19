0451d2ad0c331faf5689332cb65c80c8df831a5a
sobhedanesh <c86092326@gmail.com>
2026-06-19 21:25:37 +0330

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'hsc-chatbot-cache-v1';
+const urlsToCache = [
+  '/h-s-c-2/',
+  '/h-s-c-2/index.html',
+  '/h-s-c-2/manifest.json',
+  '/h-s-c-2/لوگو اندروید.png'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activate Service Worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch Requests
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
